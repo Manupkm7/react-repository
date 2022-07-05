999c524fa2853fa41a8195aaa9fa09efcb426f67
Manuel Espinoza <manuelespinoza238@gmail.com>
2022-07-05 20:26:48 -0300

    init project

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View } from 'react-native';
+import Main from './src/Components/Main';
+
+export default function App() {
+  return (<Main />);
+}
---
+++ src/Components/Main.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+import Constants from "expo-constants";
+import { Text, View } from "react-native";
+
+const Main = () => {
+return (
+    <View style={{ marginTop: Constants.statusBarHeight }}>
+        <Text>Rate Repository Application!</Text>
+    </View>
+)
+}
+
+export default Main;
